e7c34b730d664e0a1e8d3c570a9b75a6fb25c91c
Omkar Chaturvedi <120505878+Omkar-the-codr@users.noreply.github.com>
2024-06-14 00:30:08 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,23 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.0;
 
 contract MyToken {
-    string public tokenName = "WORLDCUP";
-    string public tokenAbbrv = "WCC";
-    uint256 public totalSupply;
+    string public name; // Token Name
+    string public symbol; // Token Symbol / Abbreviation
+    uint256 public totalSupply; // Total Supply
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) public balances; // Mapping of addresses to balances
 
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply; // Assign total supply to contract creator
+    }
+
+    // mint function
     function mint(address _to, uint256 _value) public {
-        require(_to != address(0), "Cannot mint to the zero address");
+        require(_value > 0, "Value must be greater than 0");
         totalSupply += _value;
         balances[_to] += _value;
     }
 
-
+    // burn function
     function burn(address _from, uint256 _value) public {
-        require(_from != address(0), "Cannot burn from the zero address");
-        require(balances[_from] >= _value, "Insufficient balance to burn");
+        require(_value > 0, "Value must be greater than 0");
+        require(balances[_from] >= _value, "Insufficient balance");
+
         totalSupply -= _value;
         balances[_from] -= _value;
     }
